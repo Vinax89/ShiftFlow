d96d2f5bfc2f2e8e5827a2da82ece099412dda76
Irvin Bito-on <migz89star@gmail.com>
2025-09-16 13:59:45 +0000

    - import { getApps, initializeApp, applicationDefault } from 'firebase-a

--- src/lib/admin.ts
+++ src/lib/admin.ts
@@ -2,6 +2,13 @@ import { getApps, initializeApp, applicationDefault } from 'firebase-admin/app'
 import { getAuth } from 'firebase-admin/auth'
 import { getFirestore } from 'firebase-admin/firestore'
 
-const adminApp = getApps().length ? getApps()[0] : initializeApp({ credential: applicationDefault() })
+const projectId =
+  process.env.FIREBASE_PROJECT_ID ||
+  process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID ||
+  process.env.GOOGLE_CLOUD_PROJECT
+
+const adminApp = getApps().length
+  ? getApps()[0]
+  : initializeApp({ credential: applicationDefault(), projectId })
 export const adminAuth = getAuth(adminApp)
 export const adminDb = getFirestore(adminApp)

a7f6676beea587944f561be3df4dfc59462c3558
Irvin Bito-on <migz89star@gmail.com>
2025-09-16 10:55:27 +0000

    yes, make sure all of SPEC-001 is implemented fully end-to-end

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,6 +25,7 @@ export default function RootLayout({
           href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&family=Nunito+Sans:wght@700;800;900&display=swap"
           rel="stylesheet"
         />
+        <link rel="preconnect" href="https://picsum.photos" />
       </head>
       <body className="font-body antialiased">
         {children}
